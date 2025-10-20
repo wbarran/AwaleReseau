dfc55ba95d9b2cf01a626fa9c07c5706a83a363a
wbarran <barran.william@gmail.com>
2025-10-20 11:15:23 +0200

    oups

--- client/client2.h
+++ client/client2.h
@@ -1,12 +1,18 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#ifdef WIN32
+
+#include <winsock2.h>
+
+#elif defined (linux)
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h> /* close */
-#include <netdb.h>  /* gethostbyname */
+#include <netdb.h> /* gethostbyname */
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
 #define closesocket(s) close(s)
@@ -21,8 +27,8 @@ typedef struct in_addr IN_ADDR;
 
 #endif
 
-#define CRLF "\r\n"
-#define PORT 1977
+#define CRLF     "\r\n"
+#define PORT     1977
 
 #define BUF_SIZE 1024
 
@@ -33,3 +39,5 @@ static int init_connection(const char *address);
 static void end_connection(int sock);
 static int read_server(SOCKET sock, char *buffer);
 static void write_server(SOCKET sock, const char *buffer);
+
+#endif /* guard */
