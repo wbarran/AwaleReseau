409a3c0b7a11457f445e567afbd73cf6820f58f3
pcharp69 <paul.charpentier69@gmail.com>
2025-11-03 09:45:48 +0100

    Ajout ServerChallenge

---
+++ Server/ServerChallenge.c
@@ -0,0 +1,57 @@
+#include "ServerChallenge.h"
+
+
+void challengeUser(Client *clients, Client sender, int actual, const char *targetName)
+{
+    char message[BUF_SIZE];
+    int found = 0;
+
+    for (int j = 0; j < actual; j++)
+    {
+        if (strcmp(clients[j].name, targetName) == 0)
+        {
+            snprintf(message, BUF_SIZE,
+                     " %s challenged you to an Awale game!\n To accept his challenge, please type {/accept %s} in your terminal. \n If you want to decline his invitation, please type {/refuse %s}.", sender.name, sender.name, sender.name);
+            write_client(clients[j].sock, message);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        snprintf(message, BUF_SIZE,
+                 "Error : User '%s' not found or not connected.\n", targetName);
+        write_client(sender.sock, message);
+    }
+}
+
+void acceptChallenge(Client *clients, Client accepter, int actual, const char* fromName)
+{
+    char message[BUF_SIZE];
+    int found = 0;
+
+    for (int j = 0; j < actual; j++)
+    {
+        if (strcmp(clients[j].name, fromName) == 0)
+        {
+            snprintf(message, BUF_SIZE,
+                     "%s accepted your invitation! The game will now begin !\n", accepter.name);
+            write_client(clients[j].sock, message);
+
+            snprintf(message, BUF_SIZE,
+                     "You accepted %s’s invitation.  The game will now begin !\n", fromName);
+            write_client(accepter.sock, message);
+
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        snprintf(message, BUF_SIZE,
+                 "User '%s' not found.\n", fromName);
+        write_client(accepter.sock, message);
+    }
+}
---
+++ Server/ServerChallenge.h
@@ -0,0 +1,16 @@
+#ifndef SERVERCHALLENGE_H
+#define SERVERCHALLENGE_H
+
+// --- Includes ---
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include "server2.h"
+#include "client2.h"
+
+void challengeUser(Client *clients, Client sender, int actual, const char *targetName);
+void acceptChallenge(Client *clients, Client accepter, int actual, const char* fromName);
+
+#endif
